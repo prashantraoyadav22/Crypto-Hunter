c6d0c2ae76000147a15a8a3433f5766c6eed60c4
Prashant Yadav <prashantraoyadav22@gmail.com>
2025-03-02 14:18:29 +0530

    Crypto Exchange
    initial commit

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAUmQ2nb3OEItSwqN0ICu-DjWMCtmmzVxA",
+  authDomain: "cryto-hunter.firebaseapp.com",
+  projectId: "cryto-hunter",
+  storageBucket: "cryto-hunter.appspot.com",
+  messagingSenderId: "307565980040",
+  appId: "1:307565980040:web:3c2b53abc82c09851a323d",
+};
+
+export default firebaseConfig;
